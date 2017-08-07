bba246b4665b26585071205e58eb1610af6fb63c
zm245258267 <245258267@qq.com>
2017-08-07 08:52:05 +0800

    目录修改

--- webmgr/common/config/bootstrap.php
+++ webmgr/common/config/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 Yii::setAlias('common', dirname(__DIR__));
-Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
-Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('proxyend', dirname(dirname(__DIR__)) . '/proxyend');
+Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/yunyingend');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
